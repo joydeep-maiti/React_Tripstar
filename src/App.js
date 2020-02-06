116627d0e5e5535db1ebba22a72ea53d5e70d273
unknown <joydeep127@gmail.com>
2020-02-06 19:43:57 +0530

    navbar and maps done

--- src/App.js
+++ src/App.js
@@ -4,20 +4,25 @@ import Navbar from './Components/Navbar/Navbar'
 import Slider from './Components/Slider/Slider'
 import Destinations from './Components/Destinations/Destinations'
 import Activities from './Components/Activities/Activities'
+import ShelterMap from './Components/MapMarker/MapMarker'
 
 class App extends Component  {
   render(){
     return (
       <div>
-        {/* <Navbar /> */}
-        
+        <Navbar />
+        <div class="sliderNav">
           <Slider />
-          <div class="destDiv">
-            <Destinations />
-          </div>
-          <div class="destDiv">
-            <Activities />
-          </div>
+        </div>
+        <div class="mapdiv">
+          <ShelterMap />
+        </div>
+        <div class="destDiv">
+          <Destinations />
+        </div>
+        <div class="destDiv">
+          <Activities />
+        </div>
           
       </div>
       
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,29 +1,26 @@
 import React from 'react'
-
+import './Navbar.css'
 
 const navbar = ()=> {
     return(
         <nav class="navbar navbar-expand-sm navbar-dark myNavClass" >
-            <a href="#" class="navbar-brand myNavTitle">eNno</a>
+            <a href="#" class="navbar-brand myNavTitle">Tripster</a>
             <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarMenu">
                 <ul class="navbar-nav ml-auto navMenu" >
                     <li class="active nav-item">
-                        <a href="#" class="nav-link myNavLink">HOME</a>
+                        <a href="#" class="nav-link myNavLink">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link myNavLink">SERVICES</a>
+                        <a href="#" class="nav-link myNavLink">Packages</a>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link myNavLink">BLOG</a>
+                        <a href="#" class="nav-link myNavLink">Tours</a>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link myNavLink">PORTFOLIO</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link myNavLink">CONTACT</a>
+                        <a href="#" class="nav-link myNavLink">Contact</a>
                     </li>
                 </ul>
             </div>
